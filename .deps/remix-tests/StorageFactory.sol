ef8b02cf1638df3e29299d99067bdaea489fcfa7
Daemonlite <khemikal2016@gmail.com>
2025-07-11 16:29:02 +0000

    additions

--- .deps/remix-tests/StorageFactory.sol
+++ .deps/remix-tests/StorageFactory.sol
@@ -10,11 +10,18 @@ contract StorageFactory {
     SimpleStorage[] public simpleStorageArray;
 
     function createSimpleStorageContract() public {
-
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
+    }
 
+    function sfStore(uint256 _simpleStorageIndex,uint256 _simpleStorageNumber) public {
+        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        simpleStorage.store(_simpleStorageNumber);
+    }
 
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        SimpleStorage simpleStorage =  SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        return simpleStorage.retrieve();
     }
 
 }
